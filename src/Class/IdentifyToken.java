c216ed547aaabc79b4aae342d047363bbe690094
LeonardGonzG <gonzalezgutierrezleonardo1@gmail.com>
2020-01-19 19:46:39 -0500

    GUI funcionando correctamente, pasar tokens

---
+++ src/Class/IdentifyToken.java
@@ -0,0 +1,24 @@
+package Class;
+
+public class IdentifyToken {
+    
+    private final String token;
+    private final String nameToken;
+
+    public IdentifyToken(String token, String nameToken) {
+        this.token = token;
+        this.nameToken = nameToken;
+    }
+
+    public String getNameToken() {
+        return nameToken;
+    }
+
+    public String getToken() {
+        return token;
+    }
+    
+    
+    
+    
+}
